6fac333c16751a49e49ef858717277c437155d38
Evaldo Klock <neto.nemesis@gmail.com>
2023-09-08 17:04:04 -0400

    fix title

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: "Home - Open PRO",
-  description: "Page description",
+  title: "Klock Tecnologia",
+  description: "Soluções em Tecnologia da Informação",
 };
 
 import Hero from "../components/hero";
